15bcd9baf2559d1d867b1f6a6cf9cd66ef02bf0c
Mahesh <93639027+mahesh6301@users.noreply.github.com>
2022-02-10 23:11:38 +0530

    Create next_palindrome_num.py

---
+++ next_palindrome_num.py
@@ -0,0 +1,13 @@
+s_num = input("Enter a Palindrome Number ")
+
+num = s_num[::-1]
+if s_num==num:
+    temp = int(s_num)
+    while True:
+        temp = temp+1
+        if str(temp) == str(temp)[::-1]:
+            print("{} is the next palindrome number.".format(temp))
+            break
+else:
+    print("{} is not a palindrome number.".format(s_num))
+        
